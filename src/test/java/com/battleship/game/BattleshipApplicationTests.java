f284968074ab5dd85735eef46156b709e57f6d96
Artur Krawczyk <artur.damian.krawczyk@gmail.com>
2021-11-28 20:12:54 +0100

    Add firt files

---
+++ src/test/java/com/battleship/game/BattleshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battleship.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
